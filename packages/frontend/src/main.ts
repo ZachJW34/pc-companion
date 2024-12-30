357eb29f93c1251562dbe9eeeb19ff59bb2561bb
Zachary Williams <zachjw34@gmail.com>
2024-12-30 11:03:12 -0600

    fix wake lock build error

--- packages/frontend/src/main.ts
+++ packages/frontend/src/main.ts
@@ -6,11 +6,11 @@ import { requestWakeLock } from "./lib/screen-lock";
 
 createApp(App).use(createPinia()).mount("#app");
 
-let wakeLock = await requestWakeLock();
-
-if (wakeLock) {
-  setTimeout(async () => {
-    console.log("[Screen Wake Lock] 30 minutes expired, releasing lock...");
-    await wakeLock.release();
-  }, 1000 * 60 * 30);
-}
+requestWakeLock().then((wakeLock) => {
+  if (wakeLock) {
+    setTimeout(async () => {
+      console.log("[Screen Wake Lock] 30 minutes expired, releasing lock...");
+      await wakeLock.release();
+    }, 1000 * 60 * 30);
+  }
+});
